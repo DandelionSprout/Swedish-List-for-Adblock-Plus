96a9dfba0623d6d1d1d98d54522976bf425b59e0
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2022-10-31 01:54:49 +0100

    Add files via upload

--- sweubo.py
+++ sweubo.py
@@ -815,6 +815,18 @@ def prepare_abp(lines) -> str:
            line
         )
 
+        line = re.sub(
+           r".*\.app\^\$third-party$", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^\|\|.*fonts.*", 
+           r"", 
+           line
+        )
+
         if is_supported_abp(line) and not line == '':
             text += line + '\r\n'
 
@@ -1243,6 +1255,18 @@ def prepare_abp(lines) -> str:
 
         line = re.sub(
            r"^/[a-z]{1,8}\$[a-ce-z]{1,}$", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r".*\.app\^\$third-party$", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^\|\|.*fonts.*", 
            r"", 
            line
         )
